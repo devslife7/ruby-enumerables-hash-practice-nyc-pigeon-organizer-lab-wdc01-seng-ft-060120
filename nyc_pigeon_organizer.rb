7591913e659929b2f0c98ae9cbdb4b52b7de4c34
Marcos Velasco <devslife7@gmail.com>
2020-05-21 12:37:29 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,34 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  hash = {}
+  
+  data.each do |key, value|
+    data[key].each do |key1, value1|
+      data[key][key1].each do |element|
+        hash[element] = {}
+      end
+    end
+  end
+  
+  hash.each do |key, value|
+    data.each do |key1, value1|
+        hash[key][key1] = []
+    end
+  end
+  
+  hash.each do |key, value|
+    data.each do |key1, value1|
+      data[key1].each do |key2, value2|
+        data[key1][key2].each do |element|
+          if key == element
+            hash[key][key1].push(key2.to_s)
+          end
+        end
+      end
+    end
+  end
+  
+  hash
 end
