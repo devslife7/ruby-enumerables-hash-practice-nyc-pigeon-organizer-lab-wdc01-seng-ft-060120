288724aa737d66afc36cd0045e4e68d6b8651d4d
Marcos Velasco <devslife7@gmail.com>
2020-05-21 23:38:33 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -18,12 +18,12 @@ def nyc_pigeon_organizer(data)
     end
   end
   
-  pigeon_list.each do |key, value|
+  pigeon_list.each do |names, value|
     data.each do |key1, value1|
       data[key1].each do |key2, value2|
         data[key1][key2].each do |element|
-          if key == element
-            pigeon_list[key][key1].push(key2.to_s)
+          if names == element
+            pigeon_list[names][key1].push(key2.to_s)
           end
         end
       end
