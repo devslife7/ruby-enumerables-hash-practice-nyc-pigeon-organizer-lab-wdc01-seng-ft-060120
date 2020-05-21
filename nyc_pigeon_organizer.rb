09745207379577334c6365e9b7da53b542a3aa26
Marcos Velasco <devslife7@gmail.com>
2020-05-21 23:33:45 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,10 +5,9 @@ def nyc_pigeon_organizer(data)
   pigeon_list = {}
   
   data.each do |key, value|
-    value.each do |color, names|
+    value.each do |inner_key, names|
       names.each do |name|
         pigeon_list[name] = {}
-        pigeon_list[name][color]
       end
     end
   end
